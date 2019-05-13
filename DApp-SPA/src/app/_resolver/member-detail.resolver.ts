4d101754e708be79bf33371219f9089463fc6160
Tom Horvath <THorvath@hymans.co.uk>
2019-05-13 16:37:50 +0100

    Fix of pic view issue on member detail

--- DApp-SPA/src/app/_resolver/member-detail.resolver.ts
+++ DApp-SPA/src/app/_resolver/member-detail.resolver.ts
@@ -5,14 +5,13 @@ import { UserService } from '../_services/user.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { AuthService } from '../_services/auth.service';
 
 @Injectable()
 export class MemberDetailResolver implements Resolve<User> {
-    constructor(private userService: UserService, private router: Router, private alertifyService: AlertifyService, private authService: AuthService) {}
+    constructor(private userService: UserService, private router: Router, private alertifyService: AlertifyService) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User> {
-        return this.userService.getUser(this.authService.decodedToken.nameid).pipe(
+        return this.userService.getUser(route.params['id']).pipe(
             catchError(error => {
                 this.alertifyService.error('Problem retrieving data');
                 this.router.navigate(['/members']);
